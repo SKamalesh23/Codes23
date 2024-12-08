c3a0e35a8b4e911a99cfda173b78320800618339
SKamalesh23 <kamaleshsuresh426@gmail.com>
2024-12-08 16:28:35 +0530

    Added Active messages Activity

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import express from "express";
 import { Server } from "socket.io";
-// import path from "path";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT || 3500;
 const app = express();
+app.use(express.static(__dirname));
 const expressServer = app.listen(PORT,()=>{
     console.log(`Listening to port ${PORT}`);
 });
@@ -14,13 +19,19 @@ const io = new Server(expressServer,{
 io.on('connection',(socket) => {
     console.log(`User ${socket.id} Connected`);
     // const user = new User();
+        socket.emit('message',`Welcome to Chat app`);
+        socket.broadcast.emit('message',`${socket.id} Connected to the room`);
     socket.on('message',(data) => {
         console.log(`Message received is ${data.toString()}`);
-        io.emit('message',`${data.userId} : ${data.text}`);
+        io.emit('message',`${socket.id.substring(0,4)} : ${data}`);
     })
     
     socket.on('disconnect',() => {
         console.log("disconnected");
     })
+    socket.on('activity',(data) =>{
+        socket.broadcast.emit('activity',`${data} is typing`);
+    })
 })
+
 
